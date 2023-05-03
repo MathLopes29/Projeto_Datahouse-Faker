d09b7abc75d6b362e4a0ca24c4fa8a60f1361973
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2023-05-03 15:43:41 -0300

    Update 04_Fornecedor Inserts.sql

--- Script SQL/Inserts/04_Fornecedor Inserts.sql
+++ Script SQL/Inserts/04_Fornecedor Inserts.sql
@@ -1,19 +1,18 @@
 /* 15 Fornecedores */
-
-INSERT INTO TB_Fornecedor VALUES ( 1, 3, 49206547900660, 92118080546, 'Roberta Lessa', 'Apto. 570 s/n Ponte Gúbio, Cruzeiro do Oeste, AC 65892-802', 1 );
-INSERT INTO TB_Fornecedor VALUES ( 2, 1, 20648641453510, 03309891258, 'Ana Sophia Regueira', '86822 Rua Liz Grotas, Itaberaí, RR 12514-517', 1 );
-INSERT INTO TB_Fornecedor VALUES ( 3, 5, 02634686587807, 98115615446, 'Rafael de Sá', 's/n Alameda Mariana, Taiúva, ES 57249-718', 0 );
-INSERT INTO TB_Fornecedor VALUES ( 4, 6, 61545495019295, 14557049474, 'Isabella Dorneles', '46564 Ponte Emanuelly Costa, Cândido Mendes, RJ 03789-787', 0 );
-INSERT INTO TB_Fornecedor VALUES ( 5, 4, 90299428890168, 03299240781, 'Suélen Campos', 'Apto. 941 7181 Rua Luana, Caetés, RO 31579-866', 0 );
-INSERT INTO TB_Fornecedor VALUES ( 6, 7, 44450435461236, 23707510971, 'Isabel Dorneles', '4851 Rodovia Isabella Coutinho, Biritiba-Mirim, PR 70060-976', 0 );
-INSERT INTO TB_Fornecedor VALUES ( 7, 1, 86919645046699, 53547874261, 'Vitor Hugo Chaves', 'Lote 17 s/n Marginal Eduarda, Macarani, AL 62541-639', 1 );
-INSERT INTO TB_Fornecedor VALUES ( 8, 10, 63602530675276, 63746815980, 'Ana Laura Sais', '68586 Marginal Suélen Almada, Cianorte, MS 08567-467', 1 );
-INSERT INTO TB_Fornecedor VALUES ( 9, 7, 89989348153784, 37248182518, 'Emanuelly da Luz', '61666 Rodovia Pedro Tobias Corrêa, Cururupu, BA 57561-327', 0 );
-INSERT INTO TB_Fornecedor VALUES ( 10, 9, 04504754014628, 01727674220, 'Breno Silveira', 's/n Alameda João Vitor de Quadros, Joinville, MS 50731-615', 1 );
-INSERT INTO TB_Fornecedor VALUES ( 11, 8, 61787594702432, 93392824362, 'Luiz Felipe Bulhões Filho', 's/n Ponte Ígor, Cromínia, AM 78285-103', 1 );
-INSERT INTO TB_Fornecedor VALUES ( 12, 10, 85118873495423, 30003455244, 'Sra. Heloísa Monteira', 'Lote 39 05939 Ponte Lorraine, Curral de Dentro, RO 05910-203', 1 );
-INSERT INTO TB_Fornecedor VALUES ( 13, 5, 70057773786940, 00304508702, 'Ian Castelo', 'Apto. 738 2044 Rua Marli, Arvoredo, PE 04257-963', 2 );
-INSERT INTO TB_Fornecedor VALUES ( 14, 3, 00853608333900, 40637617779, 'Catarina Junqueira Jr.', '31577 Ponte Lavínia Lopes, Mirangaba, SP 93586-590', 1 );
-INSERT INTO TB_Fornecedor VALUES ( 15, 10, 96032896482819, 79179838517, 'Henrique Barroso', '76790 Viela Gael Espinhosa, Duas Estradas, DF 45049-840', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 1, 2, 13836048150674, 17041090425, 'Maria Laura Marins', 'Lote 73 50280 Marginal Gabriela da Luz, Pouso Alegre, PR 45768-491', 0 );
+INSERT INTO TB_Fornecedor VALUES ( 2, 2, 19963275683228, 15249739308, 'Agatha Barbosa', '360 Avenida Daniel Moura, Santa Helena de Goiás, PI 44687-018', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 3, 1, 18780562238023, 19098322971, 'Agatha Silva', 'Sobrado 91 672 Avenida Danilo, Ibaté, CE 29816-424', 0 );
+INSERT INTO TB_Fornecedor VALUES ( 4, 3, 18531947815583, 16413165834, 'Ruan Corrêa', '5960 Alameda Pedro Lucas, Mariápolis, MT 14000-489', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 5, 4, 14439978512057, 10785740544, 'Vitor Garcês', '18278 Avenida Milena, Augustinópolis, AM 78512-691', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 6, 4, 14240753126837, 18199784628, 'Lucca Rios', 'Quadra 98 49324 Rua Antonia Rêgo, Icém, TO 26374-987', 0 );
+INSERT INTO TB_Fornecedor VALUES ( 7, 4, 19475855346390, 10761164595, 'Gustavo Beltrão', 'Quadra 05 s/n Rua Núbia, Sobradinho, MS 82528-020', 2 );
+INSERT INTO TB_Fornecedor VALUES ( 8, 3, 15927361783201, 12633480873, 'Yasmin da Mata', 'Quadra 91 s/n Avenida Sophia, Santo Antônio do Leverger, CE 63475-760', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 9, 6, 18644742120139, 15757457883, 'Bruna Álvares', 'Lote 90 81839 Marginal Oliver Rezende, São Francisco de Goiás, ES 97143-428', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 10, 3, 12702935065988, 11824325178, 'Maria Alice Monteiro', 'Casa 3 819 Rua Levi Raia, Pedras Altas, AC 99707-816', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 11, 1, 19694442534249, 15027803762, 'Benjamin Monteira', 'Lote 83 117 Travessa Levi da Fontoura, Ouro Preto, RR 92884-821', 2 );
+INSERT INTO TB_Fornecedor VALUES ( 12, 2, 18065110031308, 16510004827, 'Maria Flor Carreira Neto', 'Quadra 71 5277 Alameda Carlos Eduardo Moura, Itaú de Minas, AC 92097-554', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 13, 3, 13139602963253, 16255748577, 'Dra. Malu Rolim', '7845 Rodovia Camila Bomdia, Marilândia, AL 27951-626', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 14, 3, 12488624413423, 15062398352, 'Eduarda Hermingues', '52224 Avenida Sara, Araucária, RJ 60976-478', 1 );
+INSERT INTO TB_Fornecedor VALUES ( 15, 5, 19394421371691, 12735123084, 'Fernando Carneiro Neto', 'Casa 2 s/n Rodovia Rafaela, Carambeí, RJ 19038-515', 0 );
 
 SELECT * FROM TB_Fornecedor;
