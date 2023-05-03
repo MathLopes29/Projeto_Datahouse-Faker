db4a88659a07c1f28b1f9ac09fc7d9cac048cf7b
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2023-05-03 16:30:41 -0300

    Update CriadorItensPedido.java

--- Java_Faker/src/main/java/br/gerador/mathlopes/itenspedido/CriadorItensPedido.java
+++ Java_Faker/src/main/java/br/gerador/mathlopes/itenspedido/CriadorItensPedido.java
@@ -13,13 +13,14 @@ public class CriadorItensPedido {
 
         for (int i = 1; i < 301; i++){
             ItensPedido ip = new ItensPedido();
+            ip.setId(i);
             ip.setPed_Id(i);
             ip.setProd_Id(i);
 
             ip.setQuantidade(faker.random().nextInt(1, 30));
             ip.setValor_Total(String.valueOf(faker.number().randomDouble(2,1,200)));
 
-            String mysql = "INSERT INTO TB_ItensPedidos VALUES(" + ip.getPed_Id() + ", " + ip.getProd_Id() + ", " + ip.getQuantidade() + ", '" + ip.getValor_Total() + "' );";
+            String mysql = "INSERT INTO TB_ItensPedidos VALUES(" + ip.getId() + ", " + ip.getPed_Id() + ", " + ip.getProd_Id() + ", " + ip.getQuantidade() + ", '" + ip.getValor_Total() + "' );";
             log.info(mysql);
         }
 /*
