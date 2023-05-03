31e16d0c029962a701e9c3a88e4bfe03843ebc06
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2023-05-03 15:42:08 -0300

    Update 00_FormaPag Inserts.sql

--- Script SQL/Inserts/00_FormaPag Inserts.sql
+++ Script SQL/Inserts/00_FormaPag Inserts.sql
@@ -1,7 +1,6 @@
 /* 4 Formas de Pagamento */
-
-INSERT INTO TB_FormaPag VALUES( 2, 'Cartão',  'Debito / Credito', 1 );
 INSERT INTO TB_FormaPag VALUES( 1, 'Dinheiro',  'Inteiro', 1 );
+INSERT INTO TB_FormaPag VALUES( 2, 'Cartão',  'Debito / Credito', 1 );
 INSERT INTO TB_FormaPag VALUES( 3, 'Pix',  'Transferencia', 1 );
 INSERT INTO TB_FormaPag VALUES( 4, 'Bit Coin',  'Transferência', 0 );
 
