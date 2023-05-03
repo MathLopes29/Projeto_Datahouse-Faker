7e16587253d82cee8bde5501966901df59449db9
Matheus Lopes <98532951+MathLopes29@users.noreply.github.com>
2023-05-03 15:41:13 -0300

    Update Criação da Database - SQL SERVER.sql

--- Script SQL/Database/Criação da Database - SQL SERVER.sql
+++ Script SQL/Database/Criação da Database - SQL SERVER.sql
@@ -9,255 +9,213 @@ GO
 
 /***********************************/
 CREATE TABLE TB_Estabelecimento(
-	Id INT IDENTITY,
+	Id INT PRIMARY KEY,
 	Nome VARCHAR (255) NOT NULL,
-	Local VARCHAR (255) NOT NULL,
-	CEP CHAR (8) NOT NULL,
-
-	CONSTRAINT pk_EstbId PRIMARY KEY (Id)
-);
-GO
-
-/**/
-CREATE TABLE TB_Estoque(
-	Id INT IDENTITY,
-	FK_EstbId INT NOT NULL,
-	Espaco INT NOT NULL CHECK(Espaco IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)),
-	Quantidade INT NULL,
-	Data_Entrada DATE,
-	Data_Saida TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-	CONSTRAINT pk_EstId PRIMARY KEY (Id),
-
-	CONSTRAINT fk_EstId FOREIGN KEY (Id)
-	REFERENCES TB_Estabelecimento(Id)
-);
-GO
-
-/**/
-CREATE TABLE TB_Fornecedor(
-	Id INT IDENTITY,
-	FK_EstId INT NOT NULL,
-	CNPJ CHAR(14) NULL,
-    CPF CHAR(11) NOT NULL,
-	Nome VARCHAR(255) NOT NULL,
-	Ender VARCHAR (255) NULL,
-	Ativo INT NOT NULL CHECK(ATIVO IN (0, 1, 2)),
-	Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Não Ativo','Ativo','Pendente'))
-	
-	CONSTRAINT pk_FornId PRIMARY KEY (Id),
-	
-	CONSTRAINT fk_Est1Id FOREIGN KEY (FK_EstId) 
-	REFERENCES TB_Estoque(Id)
-);
-GO
-
-/**/
-CREATE TABLE TB_Produto(
-	Id INT IDENTITY,
-	FK_EstId INT NOT NULL,
-	Tipo VARCHAR (255) NOT NULL,
-	Nome VARCHAR (255) NOT NULL,
-	Lote CHAR(5),
-	Validade DATE NOT NULL,
-	Valor_Unitario DECIMAL (10,2) NOT NULL,
-
-	CONSTRAINT pk_ProdId PRIMARY KEY  (Id),
-	
-	CONSTRAINT fk_Est2Id FOREIGN KEY  (FK_EstId)
-	REFERENCES TB_Estoque (Id)
-);
-GO
-
-/**/
-CREATE TABLE TB_Pedido(
-	Id INT IDENTITY,
-	FK_ClientId INT NOT NULL,
-
-	CONSTRAINT pk_PedId PRIMARY KEY  (Id),
-	
-	CONSTRAINT fk_Client1Id FOREIGN KEY  (FK_ClientId)
-	REFERENCES TB_Estoque (Id)
-);
-GO
-
-/**/
-CREATE TABLE TB_ItensPedidos(
-	FK_PedId INT NOT NULL,
-	FK_ProdId INT NOT NULL,
-	Nome VARCHAR(255) NOT NULL,
-	Quantidade FLOAT NOT NULL,
-	Valor_Total DECIMAL(10,2) NOT NULL,
-
-	CONSTRAINT fk_Ped1Id FOREIGN KEY  (FK_PedId)
-	REFERENCES TB_Pedido (Id),
-	
-	CONSTRAINT fk_ProdId FOREIGN KEY  (FK_ProdId)
-	REFERENCES TB_Produto (Id)
+	Local_Ender VARCHAR (255) NOT NULL,
+	CEP CHAR (9) NOT NULL
 );
 GO
 
 /**/
 CREATE TABLE TB_Cliente(
-	Id INT IDENTITY,
+	Id INT PRIMARY KEY,
 	CPF CHAR (11) NOT NULL UNIQUE,
 	Nome VARCHAR (255) NOT NULL,
-	Data_Nascimento DATE NOT NULL,
+	Email VARCHAR (255) NOT NULL,
+	Telefone CHAR (15)NOT NULL,
+	CEP CHAR (9) NOT NULL,
+	Ender VARCHAR (255) NOT NULL,
+	Estado CHAR (255) NOT NULL,
+	Cidade VARCHAR (255) NOT NULL,
 	Ativo INT NOT NULL CHECK(ATIVO IN (0, 1, 2)),
-	Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Não Ativo','Ativo','Pendente')),
+	
+	/* Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Nï¿½o Ativo','Ativo','Pendente')), */
+);	
+GO
 
-	CONSTRAINT pk_ClientId PRIMARY KEY (Id)
+/**/
+CREATE TABLE TB_Estoque(
+	Id INT PRIMARY KEY,
+	Estab_Id INT NOT NULL,
+	Espaco INT,
+	
+	CONSTRAINT FK_EstabID1
+		FOREIGN KEY (Estab_Id)
+			REFERENCES TB_Estabelecimento(Id)
 );
 GO
 
 /**/
+CREATE TABLE TB_Fornecedor(
+	Id INT PRIMARY KEY,
+	Est_Id INT NOT NULL,
+
+	CNPJ CHAR(14) NULL,
+    CPF CHAR(11) NOT NULL,
+	Nome VARCHAR(255) NOT NULL,
+	Ender VARCHAR (255) NULL,
+	Ativo INT NOT NULL CHECK(ATIVO IN (0, 1, 2)),
+	
+	/* Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Nï¿½o Ativo','Ativo','Pendente')) */
+	
+	CONSTRAINT FK_EstID1
+		FOREIGN KEY (Est_Id) 
+			REFERENCES TB_Estoque(Id)
+);
+GO
+
+/**/
+CREATE TABLE TB_Produto(
+	Id INT PRIMARY KEY,
+	Est_Id INT NOT NULL,
+	Lote_Cod CHAR(10) NOT NULL,
+	Lote_Caixa INT NOT NULL,
+	Lote_Quantidade INT NOT NULL,
+	Data_DiaEntrada DATE,
+    Data_HoraEntrada TIME,
+    Data_Saida DATETIME,
+	Validade DATE NOT NULL,
+	Tipo VARCHAR (255) NOT NULL,
+	Nome VARCHAR (255) NOT NULL,
+	Valor_Unitario DECIMAL (10,2) NOT NULL,
+	
+	CONSTRAINT FK_EstID2 
+		FOREIGN KEY  (Est_Id)
+			REFERENCES TB_Estoque (Id)
+);
+GO
+
+/**/
+CREATE TABLE TB_Pedido(
+	Id INT PRIMARY KEY,
+	Cliente_Id INT NOT NULL,
+	
+	CONSTRAINT FK_ClienteID1 
+		FOREIGN KEY  (Cliente_Id)
+			REFERENCES TB_Cliente (Id)
+);
+GO
+
+/**/
+CREATE TABLE TB_ItensPedidos(
+	Id INT PRIMARY KEY,
+	Ped_Id INT NOT NULL,
+	Prod_Id INT NOT NULL,
+	/* Nome VARCHAR(255) NOT NULL, */
+	Quantidade FLOAT NOT NULL,
+	Valor_Total NUMERIC(10,2) NOT NULL,
+
+	CONSTRAINT FK_PedID2 
+		FOREIGN KEY  (Ped_Id)
+			REFERENCES TB_Pedido (Id),
+	
+	CONSTRAINT FK_ProdID2 	
+		FOREIGN KEY  (Prod_Id)
+			REFERENCES TB_Produto (Id)
+);
+GO
+
+
+/**/
 CREATE TABLE TB_FormaPag(
-	Id INT IDENTITY,
+	Id INT PRIMARY KEY,
 	Nome VARCHAR(255) NOT NULL,
 	Tipo VARCHAR (255) NOT NULL,
-	StatusPag INT NOT NULL CHECK(StatusPag IN (0, 1, 2)),
-	Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Não Ativo','Ativo','Pendente')),
-	CONSTRAINT pk_FormaPagId PRIMARY KEY (Id)
+	Ativo INT NOT NULL CHECK(Ativo IN (0, 1, 2)),
+	
+	/* Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Nï¿½o Ativo','Ativo','Pendente')), */ 
 );
 GO
 
 /**/
 CREATE TABLE TB_NotaFiscal(
-	Id INT IDENTITY,
-	FK_PedId INT NOT NULL,
-	FK_ClientId INT NOT NULL,
-	FK_FormaPagId INT NOT NULL,
-    DataTemp TIMESTAMP NOT NULL,
-	Valor_Total NUMERIC (10,2) NOT NULL,
-
-	CONSTRAINT pk_NotaFiscalId PRIMARY KEY (Id),
+	Id INT PRIMARY KEY,
+	ItensPed_Id INT NOT NULL,
+	Cliente_Id INT NOT NULL,
+	FormaPag_Id INT NOT NULL,
+    DataFiscal DATETIME,
 	
-	CONSTRAINT fk_Ped2Id FOREIGN KEY (FK_PedId)
-	REFERENCES TB_Pedido (Id),
+	CONSTRAINT FK_ItensPedID1
+		FOREIGN KEY (ItensPed_Id)
+			REFERENCES TB_ItensPedidos(Id),
 	
-	CONSTRAINT fk_Client2Id FOREIGN KEY (FK_ClientId)
-	REFERENCES TB_Cliente (Id),
+	CONSTRAINT FK_ClienteID2 
+		FOREIGN KEY (Cliente_Id)
+			REFERENCES TB_Cliente (Id),
 	
-	CONSTRAINT fk_FormaPag2Id FOREIGN KEY (FK_FormaPagId)
-	REFERENCES TB_FormaPag (Id)
+	CONSTRAINT FK_FormaPagID1 
+		FOREIGN KEY (FormaPag_Id)
+			REFERENCES TB_FormaPag (Id)
 );
 GO
 
 /**/
 CREATE TABLE TB_Funcionario(
-	Id INT IDENTITY,
-	FK_EstbId INT NOT NULL,
+	Id INT PRIMARY KEY,
+	Estab_Id INT NOT NULL,
 	Nome VARCHAR (255) NOT NULL,
-	CPF CHAR (9) NOT NULL UNIQUE,
-	CEP CHAR (7) NOT NULL,
-	Telefone VARCHAR (15) NOT NULL,
+	CPF CHAR (11) NOT NULL UNIQUE,
+	CEP CHAR (9) NOT NULL,
+	Telefone CHAR (15) NOT NULL,
 
-	CONSTRAINT pk_Id PRIMARY KEY (Id),
-
-	CONSTRAINT fk_Estb1Id FOREIGN KEY (FK_EstbId)
-	REFERENCES TB_Estabelecimento (Id)
-);
-GO
-
-/**/
-CREATE TABLE TB_Setor(
-	Id INT IDENTITY,
-	NomeSetor VARCHAR(255) NOT NULL,
-	FK_EstbId INT NOT NULL,
-	FK_FuncId INT NOT NULL,
-	NomeFunc VARCHAR(255) NOT NULL, 
-	Operacao VARCHAR (255) NOT NULL UNIQUE,
-	Ativo INT NOT NULL CHECK(ATIVO IN (0, 1, 2)),
-	Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Não Ativo','Ativo','Pendente')),
-
-	CONSTRAINT pk_SetorId PRIMARY KEY (Id),
-
-	CONSTRAINT fk_Estb2Id FOREIGN KEY (FK_EstbId)
-	REFERENCES TB_Estabelecimento (Id),
-
-	CONSTRAINT fk_Func2Id FOREIGN KEY (FK_FuncId)
-	REFERENCES TB_Funcionario (Id)
+	CONSTRAINT FK_EstabID2 
+		FOREIGN KEY (Estab_Id)
+			REFERENCES TB_Estabelecimento (Id)
 );
 GO
 
 /**/
 CREATE TABLE TB_Caixa(
-	Id INT IDENTITY,
-	FK_FormaPagId INT NOT NULL,
-	DataCompra TIMESTAMP NOT NULL,
+	Id INT PRIMARY KEY,
+	FormaPag_Id INT NOT NULL,
+	DataCompra DATETIME,
 	StatusCaixa INT NOT NULL CHECK(StatusCaixa IN (0, 1, 2)),
-	Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Não Ativo','Ativo','Pendente')),
 
-	CONSTRAINT pk_CaixaId PRIMARY KEY (Id),
+	/* Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Nï¿½o Ativo','Ativo','Pendente')), */
 
-	CONSTRAINT fk_FormaPag3Id FOREIGN KEY (FK_FormaPagId)
-	REFERENCES TB_FormaPag (Id)
+
+	CONSTRAINT FK_FormaPagID2 
+		FOREIGN KEY (FormaPag_Id)
+			REFERENCES TB_FormaPag (Id)
 );
 GO
 
 /**/
-CREATE TABLE TB_Comissao(
-	Id INT IDENTITY,
-	FK_SetorId INT NOT NULL,
-    FK_CaixaId INT NOT NULL,
-	Porcentagem DECIMAL (10,2) NOT NULL,
+CREATE TABLE TB_Setor(
+	Id INT PRIMARY KEY,
+	Setor VARCHAR(255) NOT NULL,
+	Func_Id INT NOT NULL,
+	
+	/*
+	Nome VARCHAR(255) NOT NULL, 
+	Operacao VARCHAR (255) NOT NULL UNIQUE,
+	*/
+	
+	Ativo INT NOT NULL CHECK(ATIVO IN (0, 1, 2)),
+	
+	/* Obs VARCHAR(15) NOT NULL CHECK (Obs IN('Nï¿½o Ativo','Ativo','Pendente')), */
 
-	CONSTRAINT pk_ComisId PRIMARY KEY (Id),
 
-	CONSTRAINT fk_SetorId FOREIGN KEY (FK_SetorId)
-	REFERENCES TB_Setor (Id),
-
-	CONSTRAINT fk_CaixaId FOREIGN KEY (FK_CaixaId)
-	REFERENCES TB_Caixa (Id)
+	CONSTRAINT FK_FuncID2 
+		FOREIGN KEY (Func_Id)
+			REFERENCES TB_Funcionario (Id)
 );
 GO
 
-/****************************************/
-INSERT INTO TB_Estabelecimento VALUES
-('Empório Abrão Market I','Rua Via Firenze - Jardim Vila Paradiso - Indaiatuba','13331563'),
-('Empório Abrão Market II','Rua João Martini - Jardim Morada do Sol - Indaiatuba','13348358');
-GO
 
-SELECT * FROM TB_Estabelecimento;
-GO
+/**/
+CREATE TABLE TB_Comissao(
+	Id INT PRIMARY KEY,
+	Setor_Id INT NOT NULL,
+    Caixa_Id INT NOT NULL,
+	Porcentagem NUMERIC (10,2) NOT NULL,
 
-/***************************************/
-INSERT INTO TB_Estoque VALUES 
-(1,1,30,'2023-01-20',DEFAULT);
-GO
+	CONSTRAINT FK_SetorID1 
+		FOREIGN KEY (Setor_Id)
+			REFERENCES TB_Setor (Id),
 
-SELECT * FROM TB_Estoque;
-GO
-
-SELECT * FROM TB_Fornecedor;
-GO
-
-SELECT * FROM TB_Produto;
-GO
-
-SELECT * FROM TB_Cliente;
-GO
-
-SELECT * FROM TB_Pedido;
-GO
-
-SELECT * FROM TB_NotaFiscal;
-GO
-
-SELECT * FROM TB_FormaPag;
-GO
-
-SELECT * FROM TB_Funcionario;
-GO
-
-SELECT * FROM TB_Comissao;
-GO
-
-SELECT * FROM TB_Caixa;
-GO
-
-SELECT * FROM TB_ItensPedidos;
-GO
-
-SELECT * FROM TB_Setor;
+	CONSTRAINT FK_CaixaID1 
+		FOREIGN KEY (Caixa_Id)
+			REFERENCES TB_Caixa (Id)
+);
 GO
